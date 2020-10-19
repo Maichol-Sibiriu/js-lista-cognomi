dd3a024c064c7b6a6c766220308b12de6ebc2faa
Maichol <msibiriu@gmail.com>
2020-10-19 16:13:38 +0200

    inizio esercizio lista cognomi

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// esercizio
+// lista cognomi
+var list = ['bianchi', 'rossi', 'duzioni', 'balsano', 'giorgini'];
+// chiedi cognome
+var user = prompt("qual'e' il tuo cognome??").toLowerCase();
+// inserire il cognome utente nella lista
+list.push(user);
+console.log(list);
+// stampa lista in ordine alfabetico
