be2741ac17be99fda0e3ca6de70eac16279b5202
Maichol <msibiriu@gmail.com>
2020-10-19 18:46:17 +0200

    search

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@
 var list = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
 // chiedi cognome
 var user = prompt("qual'e' il tuo cognome??");
+var boh = user.toUpperCase();
 // inserire il cognome utente nella lista
-list.push(user);
+list.push(boh);
 console.log(list);
 // stampa lista in ordine alfabetico
 list.sort();
 console.log(list);
 // stampa posizione "umana" del cognome utente nella lista
-var index = list.indexOf(user) + 1;
+var index = list.indexOf(boh) + 1;
 console.log(index);
 // stampa a web del cognome e della sua posizione
 document.getElementById('surname').innerHTML = "il tuo cognome e' <h1 id = upper>" + user + "</h1> e la tua posizione nella lista e' " + index;
+// alert + riproposizione del prompt
