2dc4e11a70fffd58de99f001b5e7092e3bbf3bfa
Maichol <msibiriu@gmail.com>
2020-10-19 17:47:24 +0200

    aggiunta di esercizi,problemi con il sort di js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // esercizio
 // lista cognomi
-var list = ['bianchi', 'rossi', 'duzioni', 'balsano', 'giorgini'];
+var list = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
 // chiedi cognome
-var user = prompt("qual'e' il tuo cognome??").toLowerCase();
+var user = prompt("qual'e' il tuo cognome??");
 // inserire il cognome utente nella lista
 list.push(user);
 console.log(list);
@@ -12,3 +12,5 @@ console.log(list);
 // stampa posizione "umana" del cognome utente nella lista
 var index = list.indexOf(user) + 1;
 console.log(index);
+// stampa a web del cognome e della sua posizione
+document.getElementById('surname').innerHTML = "il tuo cognome e' <h1 id = upper>" + user + "</h1> e la tua posizione nella lista e' " + index;
