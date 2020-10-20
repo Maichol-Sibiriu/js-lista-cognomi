bb93d44f9ff5cde2b0399d96437443d2bd35c69e
Maichol <msibiriu@gmail.com>
2020-10-20 15:47:18 +0200

    maiuscola ok,in corso alert

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@
 var list = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
 // chiedi cognome
 var user = prompt("qual'e' il tuo cognome??");
-var boh = user.toUpperCase();
+var big = user.charAt(0).toUpperCase() + user.slice(1);
 // inserire il cognome utente nella lista
-list.push(boh);
+list.push(big);
 console.log(list);
 // stampa lista in ordine alfabetico
 list.sort();
 console.log(list);
 // stampa posizione "umana" del cognome utente nella lista
-var index = list.indexOf(boh) + 1;
+var index = list.indexOf(big) + 1;
 console.log(index);
 // stampa a web del cognome e della sua posizione
-document.getElementById('surname').innerHTML = "il tuo cognome e' <h1 id = upper>" + user + "</h1> e la tua posizione nella lista e' " + index;
+document.getElementById('surname').innerHTML = "il tuo cognome e' " + big + " e la tua posizione nella lista e' " + index;
 // alert + riproposizione del prompt
+var alert = 
