42c4d80df74200bd03ec6e770cea76ea199736ca
Maichol <msibiriu@gmail.com>
2020-10-20 17:26:36 +0200

    problemi con ripetizione richiesta cognome

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 // esercizio
+// alert + riproposizione del prompt
+// loop di richiesta
+while (user === " ") {
+  alert("devi digitare il cognome!!");
+  user = prompt("qual'e' il tuo cognome??").trim();
+}
 // lista cognomi
 var list = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
 // chiedi cognome
-var user = prompt("qual'e' il tuo cognome??");
-var big = user.charAt(0).toUpperCase() + user.slice(1);
+var user = prompt("qual'e' il tuo cognome??").trim();
+var big = user.charAt(0).toUpperCase() + user.slice(1).toLowerCase();
 // inserire il cognome utente nella lista
 list.push(big);
 console.log(list);
@@ -15,5 +21,3 @@ var index = list.indexOf(big) + 1;
 console.log(index);
 // stampa a web del cognome e della sua posizione
 document.getElementById('surname').innerHTML = "il tuo cognome e' " + big + " e la tua posizione nella lista e' " + index;
-// alert + riproposizione del prompt
-var alert = 
